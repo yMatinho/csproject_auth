d5d89bf4f55534a9b5199e5d216e13f252851df9
Matheus Acioli <matheusmaceio2013@gmail.com>
2023-10-28 18:41:15 -0300

    Init

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+define("MAIN_DIR", __DIR__.'/');
+define("SITE_URL", "http://localhost:8081/");
+
+define("DB_DATABASE", "");
+define("DB_USERNAME", "");
+define("DB_PASSWORD", "");
+define("DB_PORT", null);
+define("DB_HOST", "");
+
+spl_autoload_register(function($class) {
+    $class = str_replace("\\","/", $class);
+    if(!file_exists(MAIN_DIR.'src/' . $class . '.php'))
+        return;
+    include MAIN_DIR.'src/' . $class . '.php';
+});
